ed050ffeac261d7a5148d1b97ee303abb74742a4
Tiago Garbi Baganha <tgbmateriais@gmail.com>
2022-08-26 20:19:31 -0300

    feat(src/styles): :art: add general styles to project

---
+++ src/styles/global-styles.ts
@@ -0,0 +1,45 @@
+import { createGlobalStyle } from 'styled-components';
+
+import { ThemeType } from '../interface/@types.theme';
+
+export const GlobalStyles = createGlobalStyle<{theme: ThemeType}>`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  html {
+    font-size: 62.5%;
+    scroll-behavior: smooth;
+  }
+
+  body {
+    font-size: 1.6rem;
+    font-family: ${({ theme }) => theme.font.family.default};
+  }
+
+  h1, h2, h3, h4, h5, h6 {
+    font-family: ${({ theme }) => theme.font.family.secondary};
+    margin: ${({ theme }) => theme.spacings.large} 0;
+  }
+
+  p {
+    margin: ${({ theme }) => theme.spacings.medium} 0;
+  }
+
+  ul, ol {
+    margin: ${({ theme }) => theme.spacings.medium};
+    padding: ${({ theme }) => theme.spacings.medium};
+  }
+
+  a {
+    text-decoration: none;
+    color: inherit;
+  }
+
+  .table {
+    width: 100%;
+    overflow-y: auto;
+  }
+`;
